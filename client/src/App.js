e572f8e4b969665eee4f59d02dbdf2d4e534d0f5
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-01 21:44:21 +0200

    building the redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {Component} from 'react';
 import AppNavbar from "./components/AppNavbar"
 import ShoppingList from "./components/ShoppingList";
+
+import { Provider } from 'react-redux';
+import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import uuid from 'uuid';
 
 
-function App() {
-  return (
-    <div className="App">
-      <AppNavbar />
-      <h1>Hello Clean Human. No virus detected</h1>
-      <ShoppingList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <h1>Hello Clean Human. No virus detected</h1>
+          <ShoppingList />
+        </div>
+      </Provider> 
+    );
+  }
 }
 
 export default App;
