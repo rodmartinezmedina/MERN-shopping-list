59688caf93051abece30bc2e0188e1cd9627e7d8
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-01 20:36:49 +0200

    list works with transition. next steps: redux implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from "./components/AppNavbar"
-
+import ShoppingList from "./components/ShoppingList";
 import './App.css';
+import uuid from 'uuid';
 
 
 function App() {
   return (
     <div className="App">
-    <AppNavbar />
+      <AppNavbar />
       <h1>Hello Clean Human. No virus detected</h1>
+      <ShoppingList />
     </div>
   );
 }
