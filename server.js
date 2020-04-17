165561b479e57a89bab093b4a8608673e20466ba
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-04-17 16:49:12 +0200

    heroku postbuild written on package.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require ('path');
+
 
 const items = require('./routes/api/items');
 
@@ -23,6 +25,19 @@ mongoose
 
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+
+  })
+
+
+}
 
 
 const port = process.env.PORT || 5000;
